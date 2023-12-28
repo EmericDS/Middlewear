43222ec08b9bf1fe223b4f36b84dccd11430ecb5
EmericDS <150539870+EmericDS@users.noreply.github.com>
2023-12-29 00:48:08 +0100

    Update repository.go

--- ratings/internal/repositories/collections/repository.go
+++ ratings/internal/repositories/collections/repository.go
@@ -1,50 +1,106 @@
-package collections
+package ratings
 
 import (
-	"github.com/gofrs/uuid"
-	"middleware/example/internal/helpers"
-	"middleware/example/internal/models"
+    "github.com/gofrs/uuid"
+    "middleware/example/internal/helpers"
+    "middleware/example/internal/models"
 )
 
-func GetAllCollections() ([]models.Collection, error) {
-	db, err := helpers.OpenDB()
-	if err != nil {
-		return nil, err
-	}
-	rows, err := db.Query("SELECT * FROM collections")
-	helpers.CloseDB(db)
-	if err != nil {
-		return nil, err
-	}
+func GetAllRatings() ([]models.Rating, error) {
+    db, err := helpers.OpenDB()
+    if err != nil {
+        return nil, err
+    }
+    rows, err := db.Query("SELECT * FROM ratings")
+    helpers.CloseDB(db)
+    if err != nil {
+        return nil, err
+    }
 
-	// parsing datas in object slice
-	collections := []models.Collection{}
-	for rows.Next() {
-		var data models.Collection
-		err = rows.Scan(&data.Id, &data.Content)
-		if err != nil {
-			return nil, err
-		}
-		collections = append(collections, data)
-	}
-	// don't forget to close rows
-	_ = rows.Close()
+    ratingsList := []models.Rating{}
+    for rows.Next() {
+        var rating models.Rating
+        err = rows.Scan(&rating.ID, &rating.UserID, &rating.SongID, &rating.Rating, &rating.Comments)
+        if err != nil {
+            return nil, err
+        }
+        ratingsList = append(ratingsList, rating)
+    }
+    _ = rows.Close()
 
-	return collections, err
+    return ratingsList, nil
+}
+func GetRatingByID(id uuid.UUID) (*models.Rating, error) {
+    db, err := helpers.OpenDB()
+    if err != nil {
+        return nil, err
+    }
+    row := db.QueryRow("SELECT * FROM ratings WHERE id=?", id.String())
+    helpers.CloseDB(db)
+
+    var rating models.Rating
+    err = row.Scan(&rating.ID, &rating.UserID, &rating.SongID, &rating.Rating, &rating.Comments)
+    if err != nil {
+        return nil, err
+    }
+    return &rating, nil
 }
 
-func GetCollectionById(id uuid.UUID) (*models.Collection, error) {
-	db, err := helpers.OpenDB()
-	if err != nil {
-		return nil, err
-	}
-	row := db.QueryRow("SELECT * FROM collections WHERE id=?", id.String())
-	helpers.CloseDB(db)
+func CreateRating(newRating models.Rating) error {
+    db, err := helpers.OpenDB()
+    if err != nil {
+        return err
+    }
+    defer helpers.CloseDB(db)
 
-	var collection models.Collection
-	err = row.Scan(&collection.Id, &collection.Content)
-	if err != nil {
-		return nil, err
-	}
-	return &collection, err
+    _, err = db.Exec("INSERT INTO ratings (id, user_id, song_id, rating, comments) VALUES (?, ?, ?, ?, ?)",
+        newRating.ID, newRating.UserID, newRating.SongID, newRating.Rating, newRating.Comments)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func UpdateRating(updatedRating models.Rating) error {
+    db, err := helpers.OpenDB()
+    if err != nil {
+        return err
+    }
+    defer helpers.CloseDB(db)
+
+    result, err := db.Exec("UPDATE ratings SET user_id=?, song_id=?, rating=?, comments=? WHERE id=?",
+        updatedRating.UserID, updatedRating.SongID, updatedRating.Rating, updatedRating.Comments, updatedRating.ID)
+    if err != nil {
+        return err
+    }
+
+    rowsAffected, _ := result.RowsAffected()
+    if rowsAffected == 0 {
+        // Aucune ligne mise à jour, l'élément n'existe probablement pas
+        return helpers.ErrNotFound
+    }
+
+    return nil
+}
+
+func DeleteRating(id uuid.UUID) error {
+    db, err := helpers.OpenDB()
+    if err != nil {
+        return err
+    }
+    defer helpers.CloseDB(db)
+
+    result, err := db.Exec("DELETE FROM ratings WHERE id=?", id)
+    if err != nil {
+        return err
+    }
+
+    rowsAffected, _ := result.RowsAffected()
+    if rowsAffected == 0 {
+        // Aucune ligne supprimée, l'élément n'existe probablement pas
+        return helpers.ErrNotFound
+    }
+
+    return nil
 }
