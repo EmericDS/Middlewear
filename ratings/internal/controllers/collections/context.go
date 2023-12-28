6f46a7273f53a22fb75128655f2f812ff4d32ebb
EmericDS <150539870+EmericDS@users.noreply.github.com>
2023-12-29 00:14:32 +0100

    Update context.go

--- ratings/internal/controllers/collections/context.go
+++ ratings/internal/controllers/collections/context.go
@@ -1,32 +1,32 @@
-package collections
+package ratings
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/gofrs/uuid"
-	"github.com/sirupsen/logrus"
-	"middleware/example/internal/models"
-	"net/http"
+    "context"
+    "encoding/json"
+    "fmt"
+    "github.com/go-chi/chi/v5"
+    "github.com/gofrs/uuid"
+    "github.com/sirupsen/logrus"
+    "middleware/example/internal/models"
+    "net/http"
 )
 
 func Ctx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		collectionId, err := uuid.FromString(chi.URLParam(r, "id"))
-		if err != nil {
-			logrus.Errorf("parsing error : %s", err.Error())
-			customError := &models.CustomError{
-				Message: fmt.Sprintf("cannot parse id (%s) as UUID", chi.URLParam(r, "id")),
-				Code:    http.StatusUnprocessableEntity,
-			}
-			w.WriteHeader(customError.Code)
-			body, _ := json.Marshal(customError)
-			_, _ = w.Write(body)
-			return
-		}
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        ratingID, err := uuid.FromString(chi.URLParam(r, "id"))
+        if err != nil {
+            logrus.Errorf("parsing error: %s", err.Error())
+            customError := &models.CustomError{
+                Message: fmt.Sprintf("cannot parse id (%s) as UUID", chi.URLParam(r, "id")),
+                Code:    http.StatusUnprocessableEntity,
+            }
+            w.WriteHeader(customError.Code)
+            body, _ := json.Marshal(customError)
+            _, _ = w.Write(body)
+            return
+        }
 
-		ctx := context.WithValue(r.Context(), "collectionId", collectionId)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
+        ctx := context.WithValue(r.Context(), "ratingID", ratingID)
+        next.ServeHTTP(w, r.WithContext(ctx))
+    })
 }
