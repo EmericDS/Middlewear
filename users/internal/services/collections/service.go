df763cf30642f5d21442a27cb5a51a8465740612
EmericDS <150539870+EmericDS@users.noreply.github.com>
2023-12-29 00:32:52 +0100

    Ajout des services manquants

--- users/internal/services/collections/service.go
+++ users/internal/services/collections/service.go
@@ -45,3 +45,61 @@ func GetUserById(id uuid.UUID) (*models.User, error) {
 
 	return user, err
 }
+
+func CreateUser(newUser models.User) error {
+    db, err := helpers.OpenDB()
+    if err != nil {
+        return err
+    }
+    defer helpers.CloseDB(db)
+
+    _, err = db.Exec("INSERT INTO users (id, username, email) VALUES (?, ?, ?)",
+        newUser.ID, newUser.Username, newUser.Email)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+func UpdateUser(updatedUser models.User) error {
+    db, err := helpers.OpenDB()
+    if err != nil {
+        return err
+    }
+    defer helpers.CloseDB(db)
+
+    result, err := db.Exec("UPDATE users SET username=?, email=? WHERE id=?",
+        updatedUser.Username, updatedUser.Email, updatedUser.ID)
+    if err != nil {
+        return err
+    }
+
+    rowsAffected, _ := result.RowsAffected()
+    if rowsAffected == 0 {
+        // Aucune ligne mise à jour, l'élément n'existe probablement pas
+        return helpers.ErrNotFound
+    }
+
+    return nil
+}
+
+func DeleteUser(id uuid.UUID) error {
+    db, err := helpers.OpenDB()
+    if err != nil {
+        return err
+    }
+    defer helpers.CloseDB(db)
+
+    result, err := db.Exec("DELETE FROM users WHERE id=?", id)
+    if err != nil {
+        return err
+    }
+
+    rowsAffected, _ := result.RowsAffected()
+    if rowsAffected == 0 {
+        // Aucune ligne supprimée, l'élément n'existe probablement pas
+        return helpers.ErrNotFound
+    }
+
+    return nil
+}
