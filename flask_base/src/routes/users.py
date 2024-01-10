40092cc1588af3d0c562f837ab06808ba477c489
EmericDS <150539870+EmericDS@users.noreply.github.com>
2024-01-10 21:21:48 +0100

    user à jour

--- flask_base/src/routes/users.py
+++ flask_base/src/routes/users.py
@@ -1,12 +1,17 @@
 import json
-from flask import Blueprint, request
-from flask_login import login_required
+from flask import Blueprint, request, jsonify
 from marshmallow import ValidationError
-
+from flask_login import login_required
 from src.models.http_exceptions import *
 from src.schemas.user import UserUpdateSchema
 from src.schemas.errors import *
-import src.services.users as users_service
+from src.services.collections import (
+    create_user,
+    delete_user,
+    get_user,
+    get_users,
+    update_user,
+)
 
 # from routes import users
 users = Blueprint(name="users", import_name=__name__)
@@ -51,7 +56,12 @@ def get_user(id):
       tags:
           - users
     """
-    return users_service.get_user(id)
+    # Appeler la fonction correspondante de l'API Users pour obtenir un utilisateur par ID
+    user = get_user(id)
+    if user:
+        return jsonify(user), 200
+    else:
+        return jsonify({"message": "User not found"}), 404
 
 
 @users.route('/<id>', methods=['PUT'])
@@ -105,16 +115,16 @@ def put_user(id):
       tags:
           - users
     """
-    # parser le body
+    # Parser le corps de la requête
     try:
         user_update = UserUpdateSchema().loads(json_data=request.data.decode('utf-8'))
     except ValidationError as e:
         error = UnprocessableEntitySchema().loads(json.dumps({"message": e.messages.__str__()}))
         return error, error.get("code")
 
-    # modification de l'utilisateur (username, nom, mot de passe, etc.)
+    # Modification de l'utilisateur (username, nom, mot de passe, etc.)
     try:
-        return users_service.modify_user(id, user_update)
+        return update_user(id, user_update)
     except Conflict:
         error = ConflictSchema().loads(json.dumps({"message": "User already exists"}))
         return error, error.get("code")
