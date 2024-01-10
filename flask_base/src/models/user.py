343687f6117df73d81ecb7dd98ed0acf707f280c
EmericDS <150539870+EmericDS@users.noreply.github.com>
2024-01-10 22:34:59 +0100

    Update user.py

--- flask_base/src/models/user.py
+++ flask_base/src/models/user.py
@@ -1,10 +1,7 @@
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from src.helpers import db
 
-
-# modèle de donnée pour la base de donnée utilisateur
-# vous pouvez lier les utilisateurs de cette API à ceux de la vôtre (Golang) avec leur ID ou leur username
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
@@ -12,15 +9,21 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255), unique=True, nullable=False)
     encrypted_password = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, uuid, username, encrypted_password):
+    def __init__(self, uuid, username, password):
         self.id = uuid
         self.username = username
-        self.encrypted_password = encrypted_password
+        self.set_password(password)
 
     def is_empty(self):
         return (not self.id or self.id == "") and \
                (not self.username or self.username == "") and \
                (not self.encrypted_password or self.encrypted_password == "")
+
+    def set_password(self, password):
+        self.encrypted_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.encrypted_password, password)
 
     @staticmethod
     def from_dict_with_clear_password(obj):
