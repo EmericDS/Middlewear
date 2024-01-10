20b88ad6e7eae2e76fd2c4ee470c2ffd54cc4336
EmericDS <150539870+EmericDS@users.noreply.github.com>
2024-01-10 22:41:23 +0100

    Update users.py

--- flask_base/src/repositories/users.py
+++ flask_base/src/repositories/users.py
@@ -1,27 +1,20 @@
 from src.helpers import db
 from src.models.user import User
 
-
-def get_user(username):
-    return db.session.query(User).filter(User.username == username).first()
-
-
-def get_user_from_id(id):
-    return User.query.get(id)
-
-
-def add_user(user):
+def create_user(user):
     db.session.add(user)
     db.session.commit()
 
-
-def update_user(user):
-    existing_user = get_user_from_id(user.id)
-    existing_user.username = user.username
-    existing_user.encrypted_password = user.encrypted_password
+def delete_user(user_id):
+    db.session.query(User).filter(User.id == user_id).delete()
     db.session.commit()
 
+def get_user(user_id):
+    return db.session.query(User).get(user_id)
 
-def delete_user(id):
-    db.session.delete(get_user_from_id(id))
+def get_users():
+    return db.session.query(User).all()
+
+def update_user(user_id, updated_user):
+    db.session.query(User).filter(User.id == user_id).update(updated_user)
     db.session.commit()
