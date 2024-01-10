f9b3dd87dbf37d9836ba8ec0d9e80b88de40f862
EmericDS <150539870+EmericDS@users.noreply.github.com>
2024-01-10 21:58:15 +0100

    ajout des cruds manquants users.py

--- flask_base/src/routes/users.py
+++ flask_base/src/routes/users.py
@@ -66,7 +66,7 @@ def get_user(id):
 
 @users.route('/<id>', methods=['PUT'])
 @login_required
-def put_user(id):
+def update_user(id):
     """
     ---
     put:
@@ -136,4 +136,127 @@ def put_user(id):
         return error, error.get("code")
     except Exception:
         error = SomethingWentWrongSchema().loads("{}")
+        return error, error.get("code")
+
+
+@users.route('/', methods=['POST'])
+def create_user():
+    """
+    ---
+    post:
+      description: Creating a new user
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema: User
+      responses:
+        '201':
+          description: Created
+          content:
+            application/json:
+              schema: User
+            application/yaml:
+              schema: User
+        '400':
+          description: Bad request
+          content:
+            application/json:
+              schema: BadRequest
+            application/yaml:
+              schema: BadRequest
+        '409':
+          description: Conflict
+          content:
+            application/json:
+              schema: Conflict
+            application/yaml:
+              schema: Conflict
+      tags:
+          - users
+    """
+    try:
+        return users_service.create_user(request)
+    except BadRequest:
+        error = BadRequestSchema().loads(json.dumps({"message": "Bad Request"}))
+        return error, error.get("code")
+    except Conflict:
+        error = ConflictSchema().loads(json.dumps({"message": "User already exists"}))
+        return error, error.get("code")
+    except Exception:
+        error = InternalServerErrorSchema().loads(json.dumps({"message": "Internal Server Error"}))
+        return error, error.get("code")
+
+
+@users.route('/<id>', methods=['DELETE'])
+def delete_user(id):
+    """
+    ---
+    delete:
+      description: Deleting a user
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of user id
+      responses:
+        '204':
+          description: No content
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+        '500':
+          description: Internal server error
+          content:
+            application/json:
+              schema: InternalServerError
+            application/yaml:
+              schema: InternalServerError
+      tags:
+          - users
+    """
+    try:
+        return users_service.delete_user(id)
+    except NotFound:
+        error = NotFoundSchema().loads(json.dumps({"message": "User not found"}))
+        return error, error.get("code")
+    except Exception:
+        error = InternalServerErrorSchema().loads(json.dumps({"message": "Internal Server Error"}))
+        return error, error.get("code")
+
+
+@users.route('/', methods=['GET'])
+def get_users():
+    """
+    ---
+    get:
+      description: Getting all users
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: List[User]
+            application/yaml:
+              schema: List[User]
+        '500':
+          description: Internal server error
+          content:
+            application/json:
+              schema: InternalServerError
+            application/yaml:
+              schema: InternalServerError
+      tags:
+          - users
+    """
+    try:
+        return users_service.get_users()
+    except Exception:
+        error = InternalServerErrorSchema().loads(json.dumps({"message": "Internal Server Error"}))
         return error, error.get("code")
