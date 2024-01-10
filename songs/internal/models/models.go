27c43c5f2a4ebb99c9c596963cff43bc021e550a
EmericDS <150539870+EmericDS@users.noreply.github.com>
2024-01-10 22:17:33 +0100

    changement de la stuct songs

--- songs/internal/models/models.go
+++ songs/internal/models/models.go
@@ -8,6 +8,6 @@ type Song struct {
 	ID          *uuid.UUID `json:"id"`
 	Title       string     `json:"title"`
 	Artist      string     `json:"artist"`
-	Album       string     `json:"album"`
-	ReleaseYear int        `json:"release_year"`
+	Genre       string     `json:"album"`
+	Duration    int        `json:"duration"`
 }
